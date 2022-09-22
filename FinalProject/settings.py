06f0e1427e980e743a0fb142ad5f3f9284649d4c
ciprian_mihu <ciprian.dorin.mihu@gmail.com>
2022-09-22 15:39:29 +0200

    settings update

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -180,7 +180,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR), 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
