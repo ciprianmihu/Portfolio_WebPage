e43c234d812cbcbbfb7c08cd9c4e21ed4141a310
ciprian_mihu <ciprian.dorin.mihu@gmail.com>
2022-09-22 14:53:00 +0200

    final settings update

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -103,14 +103,12 @@ WSGI_APPLICATION = 'FinalProject.wsgi.application'
 
 # DATABASES = {
 #     'default': {
-
 #         'ENGINE': os.environ['DB_ENGINE'],
 #         'NAME': os.environ['DB_NAME'],
 #         'USER': os.environ['DB_USER'],
 #         'PASSWORD': os.environ['DB_PASSWORD'],
 #         'HOST': os.environ['DB_HOST'],
 #         'PORT': os.environ['DB_PORT'],
-#
 #     }
 # }
 
