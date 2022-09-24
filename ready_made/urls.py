32e323a939af3b66e0a07a1763ead0fc5b20e6ef
ciprian_mihu <ciprian.dorin.mihu@gmail.com>
2022-09-24 16:36:30 +0200

    changed media urls

--- ready_made/urls.py
+++ ready_made/urls.py
@@ -5,10 +5,9 @@ from django.urls import path
 from ready_made import views
 
 urlpatterns = [
-                  path('ready_made/', views.ReadyMadeListView.as_view(), name='ready-made'),
-                  path('create_ready_logo/', views.ReadeMadeCreateView.as_view(), name='create-ready-logo'),
-                  path('update_ready_logo/<int:pk>/', views.ReadeMadeUpdateView.as_view(), name='update-ready-logo'),
-                  path('detail_ready_logo/<int:pk>/', views.ReadyMadeDetailView.as_view(), name='detail-ready-logo'),
-                  path('delete_ready_logo_modal/<int:pk>/', views.delete_ready_logo, name='delete-ready-logo-modal'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
-                                                                                         document_root=settings.STATIC_ROOT)
+    path('ready_made/', views.ReadyMadeListView.as_view(), name='ready-made'),
+    path('create_ready_logo/', views.ReadeMadeCreateView.as_view(), name='create-ready-logo'),
+    path('update_ready_logo/<int:pk>/', views.ReadeMadeUpdateView.as_view(), name='update-ready-logo'),
+    path('detail_ready_logo/<int:pk>/', views.ReadyMadeDetailView.as_view(), name='detail-ready-logo'),
+    path('delete_ready_logo_modal/<int:pk>/', views.delete_ready_logo, name='delete-ready-logo-modal'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
