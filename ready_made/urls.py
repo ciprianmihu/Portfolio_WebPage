2377d91538ce52f3b44204590df06492ce4e3b02
ciprian_mihu <ciprian.dorin.mihu@gmail.com>
2022-09-24 19:49:00 +0200

    renamed ready-made templates

--- ready_made/urls.py
+++ ready_made/urls.py
@@ -6,8 +6,8 @@ from ready_made import views
 
 urlpatterns = [
     path('ready_made/', views.ReadyMadeListView.as_view(), name='ready-made'),
-    path('create_ready_logo/', views.ReadeMadeCreateView.as_view(), name='create-ready-logo'),
-    path('update_ready_logo/<int:pk>/', views.ReadeMadeUpdateView.as_view(), name='update-ready-logo'),
-    path('detail_ready_logo/<int:pk>/', views.ReadyMadeDetailView.as_view(), name='detail-ready-logo'),
-    path('delete_ready_logo_modal/<int:pk>/', views.delete_ready_logo, name='delete-ready-logo-modal'),
+    path('create_ready/', views.ReadeMadeCreateView.as_view(), name='create-ready'),
+    path('update_ready/<int:pk>/', views.ReadeMadeUpdateView.as_view(), name='update-ready'),
+    path('detail_ready/<int:pk>/', views.ReadyMadeDetailView.as_view(), name='detail-ready'),
+    path('delete_ready/<int:pk>/', views.delete_ready_logo, name='delete-ready'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ready_made/views.py
+++ ready_made/views.py
@@ -9,7 +9,7 @@ from ready_made.models import ReadyLogo
 
 
 class ReadeMadeCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
-    template_name = 'ready_made/create_ready_logo.html'
+    template_name = 'ready_made/create_ready.html'
     model = ReadyLogo
     form_class = ReadyMadeLogoForm
     success_url = reverse_lazy('ready-made')
@@ -30,17 +30,17 @@ class ReadyMadeListView(ListView):
 
 
 class ReadeMadeUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
-    template_name = 'ready_made/update_ready_logo.html'
+    template_name = 'ready_made/update_ready.html'
     model = ReadyLogo
     form_class = ReadyMadeLogoForm
     permission_required = 'ready_made.change_readylogo'
 
     def get_success_url(self):
-        return reverse('detail-ready-logo', kwargs={'pk': self.object.id})
+        return reverse('detail-ready', kwargs={'pk': self.object.id})
 
 
 class ReadyMadeDetailView(DetailView):
-    template_name = 'ready_made/detail_ready_logo.html'
+    template_name = 'ready_made/detail_ready.html'
     model = ReadyLogo
 
     def get_context_data(self, **kwargs):
