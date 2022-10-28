b3648a166e8c6f12d54767a9e353a2610fc40dc5
ciprian_mihu <ciprian.dorin.mihu@gmail.com>
2022-10-28 18:13:25 +0200

    templates comments

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,5 @@
-from django.conf import settings
-from django.contrib import messages
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from FinalProject.settings import ADMIN_USERNAME
